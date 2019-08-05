fcc5f43ad8c0e0ba76d2d0846809d91f72b0b1fc
dilshankar <dilshankar@gmail.com>
2019-08-05 12:52:51 +0530

    Index view and routes modified

--- bdc/app.js
+++ bdc/app.js
@@ -3,9 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var Web3 = require('web3');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+
+web3 = new Web3('http://localhost:8545');//Connecting to Blockchain rpc endpoint
+//Initialize the account and contract instances
+/*
+  account ="";//account Address
+  contractaddress="";//Contract Address
+  //Initialize the contract Abi
+  contractabi = [{"constant": false,"inputs": [{"name": "","type": ""},{"name": "","type": ""},{"name": "","type": ""},{"name": "","type": ""},{"name": "","type": ""}],"name": "FunctionName","outputs": [],"payable": false,"stateMutability": "nonpayable","type": "function"},{"constant": true,"inputs": [{"name": ""","type": ""}],"name": "","outputs": [{"name": "","type": ""},{"name": "","type": ""},{"name": "","type": ""},{"name": "","type": ""}],"payable": false,"stateMutability": "view","type": "function"}];
+  //Contract Instance
+  Contractinstance = new web3.eth.Contract(contractabi,contractaddress);//contract instance
+
+*/
 
 var app = express();
 
--- bdc/routes/index.js
+++ bdc/routes/index.js
@@ -3,7 +3,14 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '' });
 });
-
+router.post('/setDonor',function(req,res,next){
+  // res.send(req.body); Testing post
+  let data = req.body;
+  //using instance of the contract to set donor data variables
+  //instance.methods.setDonor(data.donorno,data.dName,data.age,data.bloodGroup,data.gender).send({from:account,gas:600000}).then((txn)=>{
+    res.send(txn);
+  // });
+})
 module.exports = router;
